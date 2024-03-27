60a107dd40c516b0da39f0fbc5a4a107b2507ccb
Yogeshceem <s572178@nwmissouri.edu>
2024-03-27 15:43:49 -0500

    commit

--- routes/Holograph.js
+++ routes/Holograph.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('holographs', { title: 'Search Results - Holographs' });
+  res.render('Holographs', { title: 'Search Results - Holographs' });
 });
 
 module.exports = router;

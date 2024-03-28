a635edcdc72af2ce45b99c272be9faa255840750
Yogeshceem <s572178@nwmissouri.edu>
2024-03-27 19:51:20 -0500

    commit

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var holographsRouter = require('./routes/holographs');
+// Import the grid router
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
+
+
 
 var app = express();
 
@@ -23,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/holographs', holographsRouter);
+// Use the grid router for the /grid endpoint
+app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

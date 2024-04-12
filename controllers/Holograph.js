a34c49d3d4d87034a136c2fa345ecd865f12931a
Yogeshceem <s572178@nwmissouri.edu>
2024-04-11 21:48:00 -0500

    commit

--- controllers/Holograph.js
+++ controllers/Holograph.js
@@ -2,26 +2,87 @@
 var Holograph = require('../models/Holograph');
 // List of all Costumes
 exports.Holograph_list = async function (req, res) {
-res.send('NOT IMPLEMENTED: Costume list');
+    try {
+        theHolograph = await Holograph.find();
+        res.send(theHolograph);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
+
 
 
 // for a specific Costume.
-exports.Holograph_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Holograph detail: ' + req.params.id);
+exports.Holograph_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Holograph.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
+
 // Handle Costume create on POST.
-exports.Holograph_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Holograph create POST');
+// Handle Holograph create on POST.
+exports.Holograph_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Holograph();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    document.holograph_name = req.body.holograph_name;
+    document.creator = req.body.creator;
+    document.edition_number = req.body.edition_number;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send({ "error": `Error: ${err}` });
+
+    }
 };
-// Handle Costume delete from on DELETE.
-exports.Holograph_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Holograph delete DELETE ' + req.params.id);
+
+// Handle Icecream delete from on DELETE.
+exports.Holograph_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Costume.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
-// Handle Costume update form on PUT.
-exports.Holograph_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Holograph update PUT' + req.params.id);
+
+//Handle Costume update form on PUT.
+exports.Holograph_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Holograph.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.holograph_name)
+            toUpdate.holograph_name = req.body.holograph_name;
+        if (req.body.creator) toUpdate.creator = req.body.creator;
+        if (req.body.edition_number) toUpdate.edition_number = req.body.edition_number;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
+
 
 // Handle a show all view
 exports.Holograph_view_all_Page = async function (req, res) {
@@ -33,7 +94,7 @@ exports.Holograph_view_all_Page = async function (req, res) {
         res.status(500);
         res.send({ "error": `Error: ${err}` });
 
-}
+    }
 };
 
 // Handle Holograph create on POST.
@@ -43,9 +104,9 @@ exports.Holograph_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    document.Holograph_flavour = req.body.Holograph_flavour;
-    document.Holograph_toppings = req.body.Holograph_toppings;
-    document.Holograph_price = req.body.Holograph_price;
+    document.holograph_name = req.body.holograph_name;
+    document.creator = req.body.creator;
+    document.edition_number = req.body.edition_number;
     try {
         let result = await document.save();
         res.send(result);
@@ -54,5 +115,47 @@ exports.Holograph_create_post = async function (req, res) {
         res.status(500);
         res.send({ "error": `Error: ${err}` });
 
+    }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Holograph_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('Holographcreate', { title: 'Holograph Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send({ 'error': '${err}' });
+    }
+};
+// Handle building the view for updating a costume.
+// query provides the id
+exports.Holograph_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Holograph.findById(req.query.id)
+        res.render('Holographupdate', { title: 'Holograph Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send({ 'error': '${err}' });
+    }
+};
+// Handle a delete one view with id from query
+exports.Holograph_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Holograph.findById(req.query.id)
+        res.render('Holographdelete', {
+            title: 'Holograph Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send({ 'error': '${err}' });
     }
 };
--- routes/holographs.js
+++ routes/holographs.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Holographs', { title: 'Search Results - holographs' });
+  res.render('Holograph', { title: 'Search Results Holograph' });
 });
+
 var express = require('express');
 const Holograph_controlers= require('../controllers/Holograph');
 var router = express.Router();
 
 router.get('/', Holograph_controlers.Holograph_view_all_Page );
 
+/* GET detail costume page */
+router.get('/detail', Holograph_controlers.Holograph_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', Holograph_controlers.Holograph_create_Page);
+
+/* GET create update page */
+router.get('/update', Holograph_controlers.Holograph_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', Holograph_controlers.Holograph_delete_Page);
 
 module.exports = router;
